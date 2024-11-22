d2ec2f1d4bd12609923568821d541ba3efe8e2e9
kavi-thagadoor <kaviarasan.m@thagadoor.org>
2024-11-22 12:37:56 +0530

    update service url

--- src/app/service/socket.service.ts
+++ src/app/service/socket.service.ts
@@ -10,7 +10,7 @@ export class SocketService {
   private socket: Socket;
 
   constructor() {
-    this.socket = io('http://localhost:3000');
+    this.socket = io('https://ws-backend-6wrs.onrender.com');
   }
 
   // Emit new user data to the server
